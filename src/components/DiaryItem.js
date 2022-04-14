297e64c7892dc63de7ee1d47f5b08d448a655415
Kyoo130 <kml9003@kakao.com>
2022-04-15 02:36:54 +0900

    :sparkles: onDelete > onRemove 함수명 변경 및 수정 기능 개발

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 
-const DiaryItem = ({ id, author, content, emotion, created_date, onDelete }) => {
+const DiaryItem = ({
+  id,
+  author,
+  content,
+  emotion,
+  created_date,
+  onRemove,
+  onEdit,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef();
+
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
   return (
     <ItemCont>
       <InfoCont>
@@ -11,13 +47,32 @@ const DiaryItem = ({ id, author, content, emotion, created_date, onDelete }) => 
         <br />
         <InfoDate>{new Date(created_date).toLocaleString()}</InfoDate>
       </InfoCont>
-      <ContentBox>{content}</ContentBox>
-      <button onClick={() => {
-        console.log(id)
-        if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
-          onDelete(id);
-        }
-      }}>삭제하기</button>
+      <ContentBox>
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => {
+                setLocalContent(e.target.value);
+              }}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </ContentBox>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </ItemCont>
   );
 };
@@ -26,22 +81,22 @@ const ItemCont = styled.div`
   background-color: rgba(240, 240, 240);
   margin-bottom: 10px;
   padding: 20px;
-`
+`;
 
 const InfoCont = styled.div`
   border-bottom: 1px solid #bdbdbd;
   padding-bottom: 10px;
   margin-bottom: 10px;
-`
+`;
 
 const InfoDate = styled.span`
   color: gray;
-`
+`;
 
 const ContentBox = styled.div`
   font-weight: bold;
   margin-top: 30px;
   margin-bottom: 30px;
-`
+`;
 
 export default DiaryItem;
--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {DiaryItem} from "./index";
+import { DiaryItem } from "./index";
 import styled from "styled-components";
 
-const DiaryList = ({ diaryList, onDelete }) => {
+const DiaryList = ({ diaryList, onRemove, onEdit }) => {
   console.log(diaryList);
   return (
     <ListCont>
@@ -10,7 +10,7 @@ const DiaryList = ({ diaryList, onDelete }) => {
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+          <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit} />
         ))}
       </div>
     </ListCont>
@@ -19,16 +19,16 @@ const DiaryList = ({ diaryList, onDelete }) => {
 
 DiaryList.defaultProps = {
   diaryList: [],
-}
+};
 
 const ListCont = styled.div`
   border: 1px solid #bdbdbd;
   padding: 20px;
   margin-top: 20px;
-  
+
   & h2 {
     text-align: center;
   }
-`
+`;
 
 export default DiaryList;
